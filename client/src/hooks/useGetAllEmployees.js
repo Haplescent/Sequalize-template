71b38c31c8f9649100168faeb94e193853ddf425
John Merritt <johnmerritt95@gmail.com>
2020-08-11 13:17:58 -0700

    useEffect hook working

--- client/src/hooks/useGetAllEmployees.js
+++ client/src/hooks/useGetAllEmployees.js
@@ -1,25 +1,4 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
-
 const useGetAllEmployees = () => {
-  const [employeeList, setEmployeeList] = useState([]);
-  useEffect(() => {
-    let config = {
-      method: "get",
-      url: "https://secret-castle-21752.herokuapp.com/api/all",
-      headers: {},
-    };
-
-    axios(config)
-      .then(function (response) {
-        let jsonString = JSON.stringify(response.data);
-        let jsonObject = JSON.parse(jsonString);
-        setEmployeeList(jsonObject);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
   return employeeList;
 };
 
