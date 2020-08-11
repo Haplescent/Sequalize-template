b3440915d0331993e04c0713a7210374ca4e4953
John Merritt <johnmerritt95@gmail.com>
2020-08-11 13:48:15 -0700

    fixed sendFile bug

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require("path");
 
 router.get("/", (req, res) => {
-  res.send(path.resolve(__dirname, "../client/public/index.html"));
+  res.sendFile(path.resolve(__dirname, "../client/public/index.html"));
 });
 
 module.exports = router;
