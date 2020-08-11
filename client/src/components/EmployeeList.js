33e74b59ba4bf59be81b36fd3b7716831f0c36a4
John Merritt <johnmerritt95@gmail.com>
2020-08-11 13:22:29 -0700

    created custom useGetAllEmployee.js hook

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -1,38 +1,9 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import axios from "axios";
+
+import useGetAllEmployees from "../hooks/useGetAllEmployees.js";
 
 function EmployeeList() {
-  const [employeeList, setEmployeeList] = useState([
-    {
-      id: 4,
-      text: "example test todo",
-      createdAt: "2020-08-11T18:17:18.000Z",
-      updatedAt: "2020-08-11T18:17:18.000Z",
-    },
-  ]);
-
-  useEffect(() => {
-    const apiCall = async () => {
-      let config = {
-        method: "get",
-        url: "https://secret-castle-21752.herokuapp.com/api/all",
-        headers: {},
-      };
-
-      await axios(config)
-        .then(function (response) {
-          let jsonString = JSON.stringify(response.data);
-          setEmployeeList(JSON.parse(jsonString));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    };
-
-    console.log("doing use Effect");
-    apiCall();
-  }, []);
+  const employeeList = useGetAllEmployees();
 
   return (
     <div>
--- client/src/hooks/useGetAllEmployees.js
+++ client/src/hooks/useGetAllEmployees.js
@@ -1,4 +1,28 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const useGetAllEmployees = () => {
+  const [employeeList, setEmployeeList] = useState([]);
+
+  useEffect(() => {
+    const apiCall = async () => {
+      let config = {
+        method: "get",
+        url: "https://secret-castle-21752.herokuapp.com/api/all",
+        headers: {},
+      };
+
+      await axios(config)
+        .then(function (response) {
+          let jsonString = JSON.stringify(response.data);
+          setEmployeeList(JSON.parse(jsonString));
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    };
+    apiCall();
+  }, []);
   return employeeList;
 };
 
