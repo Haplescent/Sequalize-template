d905864c967d3a5c9402a1afb5932d20c08333f1
John Merritt <johnmerritt95@gmail.com>
2020-08-11 13:15:59 -0700

    added cores to server.js

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require("path");
 const app = express();
 const db = require(path.resolve(__dirname, "./models/index.js"));
 const PORT = process.env.PORT || 8080;
+var cors = require("cors");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 const apiRoutes = require(path.resolve(__dirname, "./routes/apiRoutes"));
 app.use("/api", apiRoutes);
