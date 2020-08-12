58dc04362e8f75365c5d8e14ee4898089cd61a6f
John Merritt <johnmerritt95@gmail.com>
2020-08-11 18:04:55 -0700

    aligned left and added employee static info

--- client/src/components/EmployeeList.js
+++ client/src/components/EmployeeList.js
@@ -23,24 +23,17 @@ import FilterListIcon from "@material-ui/icons/FilterList";
 
 import useGetAllEmployees from "../hooks/useGetAllEmployees.js";
 
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
+function createData(name, phone, address, title) {
+  return { name, phone, address, title };
 }
 
 const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
+  createData(
+    "John",
+    "6508677015",
+    "766 Harrison St, Unit 209, San Francisco, CA, 94401",
+    "Scientist"
+  ),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -74,12 +67,11 @@ const headCells = [
     id: "name",
     numeric: false,
     disablePadding: true,
-    label: "Dessert (100g serving)",
+    label: "Employee Name",
   },
-  { id: "calories", numeric: true, disablePadding: false, label: "Calories" },
-  { id: "fat", numeric: true, disablePadding: false, label: "Fat (g)" },
-  { id: "carbs", numeric: true, disablePadding: false, label: "Carbs (g)" },
-  { id: "protein", numeric: true, disablePadding: false, label: "Protein (g)" },
+  { id: "Phone", numeric: false, disablePadding: false, label: "Phone Number" },
+  { id: "Address", numeric: false, disablePadding: false, label: "Address" },
+  { id: "Title", numeric: false, disablePadding: false, label: "Title" },
 ];
 
 function EnhancedTableHead(props) {
@@ -104,7 +96,7 @@ function EnhancedTableHead(props) {
             indeterminate={numSelected > 0 && numSelected < rowCount}
             checked={rowCount > 0 && numSelected === rowCount}
             onChange={onSelectAllClick}
-            inputProps={{ "aria-label": "select all desserts" }}
+            inputProps={{ "aria-label": "select all employees" }}
           />
         </TableCell>
         {headCells.map((headCell) => (
@@ -189,7 +181,7 @@ const EnhancedTableToolbar = (props) => {
           id="tableTitle"
           component="div"
         >
-          Nutrition
+          Employees
         </Typography>
       )}
 
@@ -241,7 +233,7 @@ const useStyles = makeStyles((theme) => ({
 export default function EmployeeList() {
   const classes = useStyles();
   const [order, setOrder] = React.useState("asc");
-  const [orderBy, setOrderBy] = React.useState("calories");
+  const [orderBy, setOrderBy] = React.useState("name");
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
@@ -353,10 +345,9 @@ export default function EmployeeList() {
                       >
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="left">{row.phone}</TableCell>
+                      <TableCell align="left">{row.address}</TableCell>
+                      <TableCell align="left">{row.title}</TableCell>
                     </TableRow>
                   );
                 })}
