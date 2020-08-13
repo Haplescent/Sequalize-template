f83e101bad037ecb655e361be7173e875f48120a
John Merritt <johnmerritt95@gmail.com>
2020-08-12 17:12:51 -0700

    update featured added

--- client/src/hooks/useHandleRowUpdate.js
+++ client/src/hooks/useHandleRowUpdate.js
@@ -1,3 +1,30 @@
+var axios = require("axios");
+var qs = require("qs");
+
+const ApiPutUpdate = (newData) => {
+  var data = qs.stringify({
+    id: newData.id,
+    name: newData.name,
+    phone: newData.phone,
+    address: newData.address,
+    title: newData.title,
+  });
+  var config = {
+    method: "put",
+    url: "https://secret-castle-21752.herokuapp.com/api/edit/",
+    headers: {},
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+};
+
 const useHandleRowUpdate = (setEmployeeList) => {
   const handleOnRowUpdate = (newData, oldData) =>
     new Promise((resolve) => {
@@ -6,6 +33,7 @@ const useHandleRowUpdate = (setEmployeeList) => {
         if (oldData) {
           setEmployeeList((prevState) => {
             const data = [...prevState.data];
+            ApiPutUpdate(newData);
             data[data.indexOf(oldData)] = newData;
             return { ...prevState, data };
           });
